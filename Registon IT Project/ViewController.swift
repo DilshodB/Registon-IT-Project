e51a711edb3c47220c55bb1486a1595cbb8ee981
DilshodB <nnepke99@gmai.com>
2023-01-30 14:37:17 +0500

    Initial Commit

---
+++ Registon IT Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Registon IT Project
+//
+//  Created by 1 1 on 30/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
